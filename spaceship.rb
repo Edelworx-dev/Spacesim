dcdfb3988fe2c101edeca09974b8da47f217eddf
Joschi <jgonline@live.de>
2016-08-15 14:12:32 +0200

    Spaceship test 2

--- spaceship.rb
+++ spaceship.rb
@@ -6,12 +6,13 @@ sspunkt = 0 #schutztschild punkte
 sp = 0 #Raumschiff leben
 lp = 0 #Laser
 tp = 0 #Triebwerk
-
-  def zeigenergie(ep)
-   puts "Energie",ep
-  end
-  
+def spaceshiptest (ep1,ssp1,sspunkt1,sp1,lp1,tp1)
+ ep = ep1 
+ ssp = ssp1
+ sspunkt = sspunkt1
+ sp = sp1
+ lp =lp1
+ tp = lp1
+ puts "Energie",ep1,"schutztschild", ssp1, " Schutztschild punkt:", sspunkt1,"Raumschiff Leben:",sp1,"Laser",lp1
+ end
 end
-
-tardis = Raumschiff.new
-tardis.zeigenergie
