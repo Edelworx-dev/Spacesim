5df54e328d9f7d45684ba641045fdc128495ce62
Christian Günther <cguenther@Ratasum-2.local>
2016-08-15 13:08:27 +0200

    new meteroit

---
+++ meteroit.rb
@@ -0,0 +1,39 @@
+class Meteroit
+ mleben = 5
+ mstärke = 3
+ mspeed = 1
+ mrichtung = 0 #1 = N , 2 = O , 3 S, W=4
+ 
+ 
+
+def MeteroitTest (testleben, teststärke, testspeed )
+ mleben = testleben
+ mstärke = teststärke
+ mspeed = testspeed
+ print "mleben:",mleben," mstärke:", mstärke, " mspeed:", mspeed
+end
+
+def MSetLeben (leben)
+ mleben = leben
+end
+
+def MSetstärke(stärke)
+ mstärke = stärke
+end
+
+def MSetspeed (speed)
+ mspeed = speed
+end
+
+def MSetrichtung (richtung)
+ mrichtung = speed
+end
+
+
+
+
+
+end
+# class end Meteroit
+meteroit1 = Meteroit.new
+meteroit1.MeteroitTest(10,1,2)
