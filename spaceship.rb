aec8ede93de0565f666b2e13eff873455c253fc3
Joschi <jgonline@live.de>
2016-08-15 13:40:26 +0200

    Spaceship test update

--- spaceship.rb
+++ spaceship.rb
@@ -1,1 +1,17 @@
-puts "egal"
+#require "colorize"
+class Raumschiff
+ep = 0 # energiepunkte
+ssp = 0 #schutztschild
+sspunkt = 0 #schutztschild punkte
+sp = 0 #Raumschiff leben
+lp = 0 #Laser
+tp = 0 #Triebwerk
+
+  def zeigenergie(ep)
+   puts "Energie",ep
+  end
+  
+end
+
+tardis = Raumschiff.new
+tardis.zeigenergie
